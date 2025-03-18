e2335d9ed1265cdb612fc5d1f58a418274ac08b1
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-03-18 04:07:19 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
-      axios: 'axios/dist/axios.min.js',  // Alias opcional, se necessário
-    }
-  }
+      '@': path.resolve(__dirname, 'src'), // Usando o @ para facilitar os imports
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Substitua pela URL do seu backend se necessário
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
