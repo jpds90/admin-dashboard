255e0bc6a0ac6ec009551770667617a2fe738cfb
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-03-18 03:26:54 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  },
-  build: {
-    rollupOptions: {
-      external: ['axios'], // Garantir que o axios não cause problemas na build
-    }
   }
 });
