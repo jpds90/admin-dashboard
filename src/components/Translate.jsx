ee5452121f05a29ace59fe3139bd59318d1b9f95
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-03-19 15:23:26 +0000

    Update Translate.jsx

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -10,17 +10,24 @@ const Translate = () => {
 
         window.googleTranslateElementInit = () => {
             new window.google.translate.TranslateElement(
-                { 
-                    pageLanguage: "pt", 
-                    includedLanguages: "en,es,pt,fr", // Restringe as línguas disponíveis
-                    layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE 
+                {
+                    pageLanguage: "pt",
+                    includedLanguages: "en,es,pt,fr", // Apenas Inglês, Espanhol, Português e Francês
+                    layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
                 },
                 "google_translate_element"
             );
         };
     }, []);
 
-    return <div id="google_translate_element"></div>;
+    return (
+        <div className="relative">
+            <div
+                id="google_translate_element"
+                className="bg-white text-gray-800 px-3 py-1 rounded-lg shadow-md cursor-pointer"
+            ></div>
+        </div>
+    );
 };
 
 export default Translate;
